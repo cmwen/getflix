cefca36b070842eb167e8c8b311b184002d403b3
cmwen <cmwen@users.noreply.github.com>
2022-05-23 12:17:19 +0000

    deploy: 43ce99845ca574ea94eee6330c125809590fff5c

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./Header";
-export * from "./GetFlixLogo";
-export * from "./MovieCard";
-export * from "./MovieDetail";
-export * from "./MovieList";
-export * from "./ThemeToggler";
