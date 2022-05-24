afd2ce6789659768b5ca52a9798d7cb3f93c584b
Min <cmwen@users.noreply.github.com>
2022-05-24 12:28:01 +0000

    Feat: add unit tests

--- src/components/GetFlixLogo.test.tsx
+++ src/components/GetFlixLogo.test.tsx
@@ -3,16 +3,30 @@ import { render, screen } from "@testing-library/react";
 import GetFlixLogo from "./GetFlixLogo";
 import { MemoryRouter } from "react-router-dom";
 
-test("Logo should render", () => {
-  render(
-    <MemoryRouter>
-      <GetFlixLogo
-        onClick={() => {
-          jest.fn();
-        }}
-      />
-    </MemoryRouter>
-  );
-  const linkElement = screen.getByText(/Get Flix/i);
-  expect(linkElement).toBeInTheDocument();
+describe("GetFlixLogo", () => {
+  test("Logo should render", () => {
+    render(
+      <MemoryRouter>
+        <GetFlixLogo
+          onClick={() => {
+            jest.fn();
+          }}
+        />
+      </MemoryRouter>
+    );
+    const linkElement = screen.getByText(/Get Flix/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("Clicking on logo should call onClick", () => {
+    const onClick = jest.fn();
+    render(
+      <MemoryRouter>
+        <GetFlixLogo onClick={onClick} />
+      </MemoryRouter>
+    );
+    const linkElement = screen.getByText(/Get Flix/i);
+    linkElement.click();
+    expect(onClick).toHaveBeenCalled();
+  });
 });
