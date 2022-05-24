ec0f5af77b20193b6945c1d7fde5c506fb7bde87
Min <cmwen@users.noreply.github.com>
2022-05-24 11:53:05 +0000

    Feat: add stories for some components

--- src/components/GetFlixLogo.test.tsx
+++ src/components/GetFlixLogo.test.tsx
@@ -6,7 +6,11 @@ import { MemoryRouter } from "react-router-dom";
 test("Logo should render", () => {
   render(
     <MemoryRouter>
-      <GetFlixLogo onClick={() => {jest.fn()}} />
+      <GetFlixLogo
+        onClick={() => {
+          jest.fn();
+        }}
+      />
     </MemoryRouter>
   );
   const linkElement = screen.getByText(/Get Flix/i);
